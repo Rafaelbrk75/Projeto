bf6b26acd76888041209cd0681701bfec7d25775
Rafaelbrk75 <rafaelbrk75@gmail.com>
2024-10-14 16:15:41 -0300

    update

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $('#mobile_btn').on('click', function () {
+    $('#mobile_menu').toggleClass('active');
+  });
+});
+
+const url = 'http://127.0.0.1:3333';
+const cadastro = document.getElementById('cadastro');
+const bnt = document.getElementById('bnt');
+
+const token = 'cadastro';
+
+const myHeaders = new Headers({
+  'Authorrization': `Bearer ${token}`
+});
+
+const myInit = {
+  method: 'GET',
+  headers: myHeaders
+};
+
+fetch (url).then(response => {
+  return response.json();
+})
+.then(data => {
+  const { id } = data.id
+  console.log(id);
+});
+
+function trazer(){
+  
+  axios
+      .get(url)
+  .then(response => {
+    const imagesContainer = document.getElementById('images-container');
+    
+    console.log("Dados da API:", response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao consumir API:', error);
+  });
+
+
+}
+
+trazer();
+
+document.getElementById('buttonL').addEventListener('click', () => {
+  window.location.href = "./login/login.html";
+});
